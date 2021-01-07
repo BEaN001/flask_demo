9825f6f958160e41ebb072096a6199d0d7e4f272
Bin Yu <binyu17812@gmail.com>
2021-01-07 13:56:35 +0800

    gg

---
+++ flaskr.py
@@ -0,0 +1,11 @@
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+# def connect_db():
+#     """Connects to the specific database."""
+#     rv = sqlite3.connect(app.config['DATABASE'])
+#     rv.row_factory = sqlite3.Row
+#     return rv
+
